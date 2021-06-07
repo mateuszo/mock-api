e5f77d4356b21f7cd349c0cc7060fbe4dfde6cb9
Mateusz Ostafil <mostafil@divante.com>
2021-06-07 15:12:12 +0200

    Move product data to common folder.

--- src/products-v2/products-2.service.ts
+++ src/products-v2/products-2.service.ts
@@ -1,43 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from '@spartacus/core/src/model/product.model';
-import { PRODUCT_DATA } from './product-v2.data';
+import { PRODUCT_DATA } from 'src/data/product.data';
 
 @Injectable()
 export class ProductsV2Service {
-
   getProducts() {
     return this.addProperty(this.denormalize(PRODUCT_DATA));
   }
 
   private denormalize(products) {
-    return products.map(
-      (product: Product) => {
-        const newProduct: any = { ...product };
-        newProduct.price = {
-          currency: product.price.currencyIso,
-          unitaryPrice: product.price.value
-        };
-        newProduct.title = product.name;
-        delete newProduct.name;
-        return newProduct;
-      }
-    );
+    return products.map((product: Product) => {
+      const newProduct: any = { ...product };
+      newProduct.price = {
+        currency: product.price.currencyIso,
+        unitaryPrice: product.price.value,
+      };
+      newProduct.title = product.name;
+      delete newProduct.name;
+      return newProduct;
+    });
   }
 
   private addProperty(products) {
-    return products.map(
-      product => ({ ...product, tags: this.generateTags(product) })
-    );
+    return products.map(product => ({
+      ...product,
+      tags: this.generateTags(product),
+    }));
   }
 
   private generateTags(product: Product): string[] {
-    const availableTags =
-      ['#new', '#old', '#limited_offer',
-        '#last_items', '#nofilter', '#spartacus',
-        '#divante', '#training', '#best', '#angular'];
+    const availableTags = [
+      '#new',
+      '#old',
+      '#limited_offer',
+      '#last_items',
+      '#nofilter',
+      '#spartacus',
+      '#divante',
+      '#training',
+      '#best',
+      '#angular',
+    ];
 
     const lastTwoDigits = (Number(product.code) % 100).toString();
     return [availableTags[lastTwoDigits[0]], availableTags[lastTwoDigits[1]]];
   }
-
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,29 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from '@spartacus/core/src/model/product.model';
-import { PRODUCT_DATA } from './product.data';
+import { PRODUCT_DATA } from '../data/product.data';
 
 @Injectable()
 export class ProductsService {
-
   getProducts() {
     return this.addProperty(this.denormalize(PRODUCT_DATA));
   }
 
   private denormalize(products) {
-    return products.map(
-      (product: Product) => {
-        const newProduct: any = { ...product };
-        newProduct.sku = product.code;
-        delete newProduct.code;
-        return newProduct;
-      }
-    );
+    return products.map((product: Product) => {
+      const newProduct: any = { ...product };
+      newProduct.sku = product.code;
+      delete newProduct.code;
+      return newProduct;
+    });
   }
 
   private addProperty(products) {
-    return products.map(
-      product => ({ ...product, color: this.getProductColor(product) })
-    );
+    return products.map(product => ({
+      ...product,
+      color: this.getProductColor(product),
+    }));
   }
 
   private getProductColor(product: Product): string {
@@ -32,6 +30,9 @@ export class ProductsService {
   }
 
   private weakHash(source: string): number {
-    return source.split('').map(char => char.charCodeAt(0)).reduce((acc, curr) => acc + curr);
+    return source
+      .split('')
+      .map(char => char.charCodeAt(0))
+      .reduce((acc, curr) => acc + curr);
   }
 }
